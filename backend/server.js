cd2698848d628e43b7c4e2932ad1b1056b0fc063
souravrane <sourav1121997@gmail.com>
2022-04-19 14:58:47 +0530

    routes and controller base created

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+
+const userRoutes = require("./routes/userRoutes");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +10,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.status(201).json({ message: "Welcome to support desk api" });
 });
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT} `);
