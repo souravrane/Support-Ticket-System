c221149991b5fde0fb5c1098bc75b21ef392cb37
souravrane <sourav1121997@gmail.com>
2022-04-20 23:45:59 +0530

    auth middle ware added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,21 @@ const generateToken = (id) => {
     });
 };
 
+// @desc Get current user
+// @route /api/users/me
+// @accss Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+    };
+
+    res.status(200).json(user);
+});
+
 module.exports = {
     registerUser,
     loginUser,
+    getMe,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { loginUser, registerUser } = require("../controllers/userController");
+const {
+    loginUser,
+    registerUser,
+    getMe,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
 
 router.post("/login", loginUser);
+
+router.get("/me", protect, getMe);
 
 module.exports = router;
